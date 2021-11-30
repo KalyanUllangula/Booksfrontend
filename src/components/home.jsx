cd51e162321cebe090fe8a64120dbf214dd70ed8
KalyanUllangula <93985763+KalyanUllangula@users.noreply.github.com>
2021-11-30 22:20:26 +0530

    Added changes

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="mt-5">
+        <h1>Welcome to Library</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
